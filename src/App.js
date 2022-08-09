2b31d51ff877e737db4bcf260ae594207d3f39b8
xcv <xcv4716@naver.com>
2022-08-09 15:01:42 +0900

    1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className='App'>
-      <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className='App-link'
-          href='https://reactjs.org'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          fuck fuck Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { BrowserRouter, Route } from 'react-router-dom';
+import { Component } from 'react';
+import { Home, Login } from '../src/pages';
+class App extends Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <div>
+          <Route exact path='/' component={Login} />
+          <Route exact path='/home' component={Home} />
+        </div>
+      </BrowserRouter>
+    );
+  }
 }
 
 export default App;
---
+++ src/pages/login/Login.js
@@ -0,0 +1,19 @@
+import { Component } from 'react';
+
+class Login extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div>
+        <h>asdasd</h>
+        <div>asdasd</div>
+      </div>
+    );
+  }
+}
+
+export default Login;
